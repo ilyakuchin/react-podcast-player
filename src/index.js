57030e662accf0bc5ff2b30672d445ab57b57b44
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-04-25 19:33:16 +0300

    Add audio tag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({ audioUrl }) => {
+  return (
+    <audio controls>
+      <source src={audioUrl} type='audio/mp4' />
+    </audio>
+  )
 }
